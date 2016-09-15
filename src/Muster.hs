366df172c7be7a6420c859797cd8130f0d8cf1d5
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-09-15 22:16:56 +0200

    remove normalize
    canonicalization should have the same effect

--- src/Muster.hs
+++ src/Muster.hs
@@ -45,42 +45,19 @@ isNullable (And l r) = isNullable l && isNullable r
 isNullable (Not o) = B.not $ isNullable o
 
 
-derivative' :: Char -> Regex -> Regex
-derivative' _ None = None
-derivative' _ Epsilon = None
-derivative' x (Symbol y) = if x == y then Epsilon else None
-derivative' x (Concatenation l r) =
-  if isNullable l
-  then (derivative' x l <.> r) <|> derivative' x r
-  else derivative' x l <.> r
-derivative' x (KleeneStar o) = derivative' x o <.> KleeneStar o
-derivative' x (Or l r) = derivative' x l <|> derivative' x r
-derivative' x (And l r) = derivative' x l <&> derivative' x r
-derivative' x (Not o) = Not (derivative' x o)
-
-
-normalize :: Regex -> Regex
-normalize (Concatenation None _) = None
-normalize (Concatenation Epsilon r) = r
-normalize (KleeneStar o) = KleeneStar (normalize o)
-normalize (Or None r) = r
-normalize (Or l None) = l
-normalize (Or l r) =
-  if l' == r' then l' else Or l' r'
-  where l' = normalize l
-        r' = normalize r
-normalize (And l r) =
-  if l' == r' then l' else And l' r'
-  where l' = normalize l
-        r' = normalize r
-normalize (Not o) = Not (normalize o)
-normalize r = r
-
-
 -- | Produces an expression that matches all suffixes of the strings described
 --   by the regular expression that start with the given character.
 derivative :: Char -> Regex -> Regex
-derivative c = normalize . derivative' c
+derivative _ None = None
+derivative _ Epsilon = None
+derivative x (Symbol y) = if x == y then Epsilon else None
+derivative x (Concatenation l r)
+  | isNullable l = (derivative x l <.> r) <|> derivative x r
+  | otherwise    = derivative x l <.> r
+derivative x (KleeneStar o) = derivative x o <.> many o
+derivative x (Or l r) = derivative x l <|> derivative x r
+derivative x (And l r) = derivative x l <&> derivative x r
+derivative x (Not o) = not (derivative x o)
 
 
 -- | Tests whether the regular expression matches the string.
